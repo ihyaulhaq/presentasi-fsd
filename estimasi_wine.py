3ba7325e82e8e0e0fe08dd9d5be033851403be21
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2023-12-18 11:39:30 +0700

    Add files via upload

--- estimasi_wine.py
+++ estimasi_wine.py
@@ -7,7 +7,7 @@ st.title('Estimasi kualitas anggur merah dan putih di portugal')
 
 fixed_acidity = st.number_input('Input kadar asam yang tetap pada wine', step=0.000001)
 residual_sugar = st.number_input('input km sisa gula setelah fermentasi', step=0.000001)
-chlorides = st.number_input('input pajak banyaknya garam', step=0.000001)
+chlorides = st.number_input('input banyaknya garam', step=0.000001)
 density = st.number_input('input kepadataan air terhadap anggurnya', step=0.000001)
 pH = st.number_input('input pH', step=0.000001)
 alcohol = st.number_input('input persenan alkohol', step=0.000001)
